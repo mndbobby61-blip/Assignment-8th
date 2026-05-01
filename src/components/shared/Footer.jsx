b55980a240e960a2adfb5e402caac6b705b265b2
MdBobby <mndbobby61@gmail.com>
2026-05-01 18:51:53 +0600

    footer update

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,13 +1,46 @@
+import React from 'react';
+
+
 const Footer = () => {
     return (
-        <footer className="bg-black text-white p-6 mt-10 text-center">
-            <p>Contact: summer@gmail.com</p>
-            <p>Privacy Policy</p>
-            <div className="space-x-3">
-                <a href="#">Facebook</a>
-                <a href="#">Instagram</a>
-            </div>
-        </footer>
+        <div>
+            <footer className="bg-orange-400 text-white text-center py-12 mt-10">
+
+                <h2 className="text-3xl font-bold">Summer Store</h2>
+
+                <p className="text-[#ffffff] mt-3 max-w-xl mx-auto text-sm">
+                    Your personal shelf of meaningful connections. Browse, tend, and nurture the relationships that matter most.
+                </p>
+                <div>____________________________________________________________________________</div>
+
+                {/* Social Icons */}
+                <div className='mt-6'>Social Links</div>
+                <div className="flex justify-center gap-4 mt-6">
+                    <div className="w-10 h-10 bg-white text-black flex items-center justify-center rounded-full">
+                        <img src="/assets/instagram.png" alt="Instagram logo" className="w-5 h-5" />
+                    </div>
+                    <div className="w-10 h-10 bg-white text-black flex items-center justify-center rounded-full">
+                        <img src="/assets/facebook.png" alt="Facebook logo" className="w-5 h-5" />
+                    </div>
+                    <div className="w-10 h-10 bg-white text-black flex items-center justify-center rounded-full">
+                        <img src="/assets/twitter.png" alt="Twitter logo" className="w-5 h-5" />
+                    </div>
+                </div>
+
+                {/* Bottom */}
+                <div className='flex justify-between px-6 flex-col md:flex-row gap-4'>
+                    <div className="mt-8 text-gray-500 text-sm flex flex-col md:flex-row justify-center gap-4">
+                        <p>© 2025 Summer Store. All rights reserved.</p>
+                    </div>
+
+                    <div className="mt-8 text-gray-500 text-sm flex flex-col md:flex-row justify-center gap-4">
+                        <p>Privacy Policy</p>
+                        <p>Terms of Service</p>
+                        <p>Contact</p>
+                    </div>
+                </div>
+            </footer>
+        </div>
     );
 };
 
